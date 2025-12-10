b2c7ff9af1f4795d39125c6f23981d96dc63c0f9
leejinhyuk0212 <myjinhyuk@tukorea.ac.kr>
2025-12-10 15:08:23 +0900

    히트박스 수정
    각 프레임 히트박스로 수정

--- game_world.py
+++ game_world.py
@@ -19,11 +19,52 @@ def handle_collisions():
     for group, pairs in collision_pairs.items():
         for a in pairs[0]:
             for b in pairs[1]:
-                if collide(a, b):
+                if _collide_objects(a, b):
                     a.handle_collision(group, b)
                     b.handle_collision(group, a)
 
+def _collide_objects(a, b):
+    a_boxes = _possible_bbs(a)
+    b_boxes = _possible_bbs(b)
 
+    for ab in a_boxes:
+        for bb in b_boxes:
+            if ab is None or bb is None:
+                continue
+            if _bb_overlap(ab, bb):
+                return True
+    return False
+
+def _possible_bbs(o):
+    bbs = []
+    try:
+        if hasattr(o, 'get_action_bb'):
+            try:
+                ab = o.get_action_bb()
+                if ab:
+                    bbs.append(ab)
+            except Exception:
+                pass
+    except Exception:
+        pass
+    try:
+        bb = o.get_bb()
+        if bb:
+            bbs.append(bb)
+    except Exception:
+        pass
+    return bbs
+
+def _bb_overlap(a_bb, b_bb):
+    left_a, bottom_a, right_a, top_a = a_bb
+    left_b, bottom_b, right_b, top_b = b_bb
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
 
 def remove_object(o):
     for layer in world:
@@ -43,17 +84,3 @@ def render():
     for layer in world:
         for o in layer:
             o.draw()
-
-def collide(a, b):
-    left_a, bottom_a, right_a, top_a = a.get_bb()
-    left_b, bottom_b, right_b, top_b = b.get_bb()
-
-    if left_a > right_b: return False
-    if right_a < left_b: return False
-    if top_a < bottom_b: return False
-    if bottom_a > top_b: return False
-
-    return True
-
-
-
